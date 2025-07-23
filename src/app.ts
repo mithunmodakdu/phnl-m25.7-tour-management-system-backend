04a56fb0f6152196049ec92e3d6d1dcd07587e3a
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-23 14:47:58 +0600

    Not Found Route middleware has been created

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import { router } from "./app/routes";
 import { envVars } from "./app/config/env";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+
 
 const app = express();
 
@@ -18,5 +20,7 @@ app.get("/", (req: Request, res:Response) =>{
 })
 
 app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
