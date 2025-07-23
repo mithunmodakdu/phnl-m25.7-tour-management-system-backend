b7c1f46dcff937a2600038b298c9588ce1749910
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-23 16:10:33 +0600

    To avoid Repetition of Try-Catch, catchAsync has been created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,27 +1,54 @@
 import { NextFunction, Request, Response } from "express";
-import httpStatusCodes, { StatusCodes } from "http-status-codes";
+import httpStatusCodes from "http-status-codes";
 import { UserServices } from "./user.service";
-import AppError from "../../errorHelpers/appError";
+// import AppError from "../../errorHelpers/appError";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
 
-    // throw new Error("fakeeeeeeeeeeee eroro");
-    // throw new AppError(StatusCodes.BAD_REQUEST, "fake error");
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
 
-    const user = await UserServices.createUser(req.body);
+//     // throw new Error("fakeeeeeeeeeeee eroro");
+//     // throw new AppError(httpStatusCodes.BAD_REQUEST, "fake error");
 
-    res.status(httpStatusCodes.CREATED).json({
-      message: "User created successfully.",
-      user,
-    });
+//     const user = await UserServices.createUser(req.body);
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-      next(error);
-  }
-};
+//     res.status(httpStatusCodes.CREATED).json({
+//       message: "User created successfully.",
+//       user,
+//     });
+
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   } catch (error: any) {
+//       next(error);
+//   }
+// };
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) =>{
+  const user = await UserServices.createUser(req.body);
+
+  res.status(httpStatusCodes.CREATED).json({
+    message: "User created successfully.",
+    user,
+  });
+
+})  
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getAllUsers = catchAsync(async(req: Request, res: Response, next: NextFunction)=>{
+  const users = await UserServices.getAllUsers();
+
+  res.status(httpStatusCodes.OK).json({
+    success: true,
+    message: "All users retrieved successfully.",
+    data: users
+  })
+  
+})
 
 export const UserControllers = {
   createUser,
+  getAllUsers
 };
