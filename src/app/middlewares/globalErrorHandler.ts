4a2ad96028ebe5956db3c87ed1df92a858b4d71c
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-23 14:30:13 +0600

    Custom Error Class AppError has been created

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,10 +2,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env"
+import AppError from "../errorHelpers/appError";
 
 export const globalErrorHandler = (error: any, req: Request, res:Response, next: NextFunction)=>{
- const statusCode = 500;
- const message = `Something Went Wrong. ${error.message}`;
+ let statusCode = 500;
+ let message = `Something Went Wrong!!`;
+
+ if(error instanceof AppError){
+    statusCode = error.statusCode;
+    message = error.message;
+
+ }else if(error instanceof Error){
+    statusCode = 500;
+    message = error.message;
+ }
  
   res.status(statusCode).json({
     success: false,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, Response } from "express";
-import httpStatusCodes from "http-status-codes";
+import httpStatusCodes, { StatusCodes } from "http-status-codes";
 import { UserServices } from "./user.service";
+import AppError from "../../errorHelpers/appError";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    
+
+    // throw new Error("fakeeeeeeeeeeee eroro");
+    // throw new AppError(StatusCodes.BAD_REQUEST, "fake error");
+
     const user = await UserServices.createUser(req.body);
 
     res.status(httpStatusCodes.CREATED).json({
