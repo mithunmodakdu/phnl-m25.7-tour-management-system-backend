c596412eb73c01c1a11f0ea2c22c364568199417
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-25 17:16:54 +0600

    checkAuth Middleware and Seed Super Admin have been completed

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,39 +1,17 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import { createUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
-import AppError from "../../errorHelpers/appError";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import { checkAuth } from "../../middlewares/checkAuth";
 import { ERole } from "./user.interface";
-import { verifyToken } from "../../utils/jwt";
-import { envVars } from "../../config/env";
 
 const router = Router();
 
-const checkAuth = (...AuthRoles: string[]) => async(req: Request, res: Response, next: NextFunction)=>{
-  try {
-    const accessToken = req.headers.authorization;
 
-    if(!accessToken){
-      throw new AppError(403, "No Access Token received.");
-    }
-
-    const verifiedAccessToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET);
-   
-    if((verifiedAccessToken as JwtPayload).role !== ERole.ADMIN){
-      throw new AppError(403, "You are not permitted to view this route");
-    }
-
-    next();
-
-  } catch (error) {
-    next(error);
-  }
-} 
 
 router.post("/register", validateRequest(createUserZodSchema), UserControllers.createUser);
 
 
-router.get("/all-users", checkAuth("ADMIN", "SUPER_ADMIN"), UserControllers.getAllUsers);
+router.get("/all-users", checkAuth(ERole.ADMIN, ERole.SUPER_ADMIN), UserControllers.getAllUsers);
 
 export const UserRoutes = router;
