a77ec0c4b7d224c4dcba11a1a8dd8c0c2f0e5b8c
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-27 20:56:43 +0600

    Creating refresh token when login and sent it to client, Implementing refresh token to get new access token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -17,6 +17,21 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
 
 }) 
 
+const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) =>{
+  // const refreshToken = req.cookies.refreshToken;
+  const refreshToken = req.headers.authorization;
+  const tokenInfo = await AuthServices.getNewAccessToken(refreshToken as string);
+  
+  sendResponse(res, {
+    statusCode: httpStatusCodes.OK,
+    success: true,
+    message: "User login successful.",
+    data: tokenInfo,
+  })
+
+}) 
+
 export const AuthControllers = {
-  credentialsLogin
+  credentialsLogin,
+  getNewAccessToken
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import AppError from "../../errorHelpers/appError";
-import { IUser } from "../user/user.interface";
+import { EIsActive, IUser } from "../user/user.interface";
 import httpStatusCodes from "http-status-codes";
 import { User } from "../user/user.model";
 import bcryptjs from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { generateToken } from "../../utils/jwt";
+import { generateToken, verifyToken } from "../../utils/jwt";
 import { envVars } from "../../config/env";
+import { createUserTokens } from "../../utils/createUserTokens";
+import { JwtPayload } from "jsonwebtoken";
 
 const credentialsLogin = async(payload: Partial<IUser>) =>{
   const {email, password} = payload;
@@ -22,6 +23,37 @@ const credentialsLogin = async(payload: Partial<IUser>) =>{
     throw new AppError(httpStatusCodes.BAD_REQUEST, "Incorrect password");
   }
 
+  const userTokens = createUserTokens(isUserExist);
+ 
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const {password: pass, ...rest} = isUserExist.toObject();
+
+  return {
+    accessToken: userTokens.accessToken,
+    refreshToken: userTokens.refreshToken,
+    user: rest
+  }
+}
+
+
+const getNewAccessToken = async(refreshToken: string) =>{
+  const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload;
+
+  const isUserExist = await User.findOne({email: verifiedRefreshToken.email});
+
+  if(!isUserExist){
+    throw new AppError(httpStatusCodes.BAD_REQUEST, "User does not exist");
+  }
+
+  if(isUserExist.isActive === EIsActive.INACTIVE || isUserExist.isActive === EIsActive.BLOCKED){
+    throw new AppError(httpStatusCodes.BAD_REQUEST, `User is ${isUserExist.isActive}`)
+  }
+
+  if(isUserExist.isDeleted){
+    throw new AppError(httpStatusCodes.BAD_REQUEST, "User is deleted")
+  }
+
   const jwtPayload = {
     userId: isUserExist._id,
     email: isUserExist.email,
@@ -30,13 +62,13 @@ const credentialsLogin = async(payload: Partial<IUser>) =>{
 
   const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
 
-
-
   return {
-    accessToken
+   accessToken
   }
 }
 
+
 export const AuthServices = {
-  credentialsLogin
+  credentialsLogin,
+  getNewAccessToken
 }
