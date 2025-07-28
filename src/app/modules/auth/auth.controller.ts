6cbd09551b557611a05f8e230320131b6461ef0d
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-28 14:57:20 +0600

    Logout User API

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -41,7 +41,32 @@ const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: N
 
 }) 
 
+const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) =>{
+  
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax"
+  })
+
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax"
+
+  })
+  
+  sendResponse(res, {
+    statusCode: httpStatusCodes.OK,
+    success: true,
+    message: "User logged out successfully.",
+    data: null,
+  })
+
+}) 
+
 export const AuthControllers = {
   credentialsLogin,
-  getNewAccessToken
+  getNewAccessToken,
+  logout
 }
