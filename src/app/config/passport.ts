09dbd2986a5b329200c3614bb3b2efe7ae5fcca6
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-28 21:54:56 +0600

    Google Authentication Configuration

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,6 +1,8 @@
 import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as GoogleStrategy, Profile, VerifyCallback } from "passport-google-oauth20";
 import { envVars } from "./env";
+import { User } from "../modules/user/user.model";
+import { ERole } from "../modules/user/user.interface";
 
 passport.use(
   new GoogleStrategy(
@@ -8,6 +10,38 @@ passport.use(
       clientID: envVars.GOOGLE_CLIENT_ID,
       clientSecret: envVars.GOOGLE_CLIENT_SECRET,
       callbackURL: envVars.GOOGLE_CALLBACK_URL
-    }, () =>{}
+    }, async(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) =>{
+      try {
+        const email = profile.emails?.[0].value;
+
+        if(!email){
+          return done(null, false, {message: "No email found."});
+        }
+
+        let user = await User.findOne({email});
+
+        if(!user){
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            picture: profile.photos?.[0].value,
+            role: ERole.USER,
+            isVerified: true,
+            auths: [
+              {
+                provider: "google",
+                providerId: profile.id
+              }
+            ]
+          });
+        }
+
+        return done(null, user);
+
+      } catch (error) {
+        console.log("Google strategy error", error);
+        return done(error);
+      }
+    }
   )
 )
