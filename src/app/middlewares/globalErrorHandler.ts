b5aa1c955f1b59b9406f0d6521ae236a217e05d3
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-08-12 15:33:17 +0600

    Handling Mongoose Validation Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,30 +6,51 @@ import AppError from "../errorHelpers/appError";
 
 export const globalErrorHandler = (error: any, req: Request, res:Response, next: NextFunction)=>{
 
-console.log(error)
+// console.log(error)
 
  let statusCode = 500;
  let message = `Something Went Wrong!!`;
+ const errorSources : any = [];
 
 //  Mongoose Duplicate error
  if(error.code === 11000){
-   console.log("Duplicate error", error.message)
+  //  console.log("Duplicate error", error.message)
    statusCode = 400;
    // message = "Duplicate error occurred"
    const duplicate = error.message.match(/"([^"]*)"/);
-   console.log(duplicate)
+  //  console.log(duplicate)
    message = `${duplicate[1]} already exists.`
  }
- //  Mongoose Cast error
+
+ //  Mongoose CastError
  else if(error.name === "CastError"){
    statusCode = 400;
    message = "Invalid MongoDB ObjectID. Please provide a valid ObjectID."
  }
+
+//  Mongoose ValidationError
+ else if(error.name === "ValidationError"){
+   statusCode = 400;
+      
+   const errors = Object.values(error.errors);
+   errors.forEach((errorObject: any) => errorSources.push(
+    {
+      path: errorObject.path,
+      message: errorObject.message
+    }
+   ));
+  //  console.log(errorSources);
+
+   message = "Validation Error";
+ }
+
  else if(error instanceof AppError){
     statusCode = error.statusCode;
     message = error.message;
 
- }else if(error instanceof Error){
+ }
+ 
+ else if(error instanceof Error){
     statusCode = 500;
     message = error.message;
  }
@@ -37,7 +58,8 @@ console.log(error)
   res.status(statusCode).json({
     success: false,
     message,
-    error,
+    errorSources,
+    // error,
     stack: envVars.NODE_ENV === "development"? error.stack : null
   })
 }
