99c33f4e247598b1396952c9fedabbfb96a6bab2
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-08-19 23:57:44 +0600

    working on create booking

--- src/app/modules/booking/booking.model.ts
+++ src/app/modules/booking/booking.model.ts
@@ -15,8 +15,7 @@ const bookingSchema = new Schema<IBooking>(
     },
     payment: {
       type: Schema.Types.ObjectId,
-      ref: "Payment",
-      required: true
+      ref: "Payment"
     },
     guestCount: {
       type: Number,
--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,7 +1,48 @@
+import AppError from "../../errorHelpers/appError";
+import { User } from "../user/user.model";
+import { EBookingStatus, IBooking } from "./booking.interface";
+import httpStatusCodes  from "http-status-codes";
+import { Booking } from "./booking.model";
+import { Payment } from "../payment/payment.model";
+import { EPaymentStatus } from "../payment/payment.interface";
+import { Tour } from "../tour/tour.model";
 
-const createBooking = async () => {
+const getTransactionId = () =>{
+  return `tran_${Date.now()}_${Math.floor(Math.random()*1000)}`
+}
 
-    return {}
+const createBooking = async (payload: Partial<IBooking>, userId: string) => {
+  const transactionId = getTransactionId();
+  const user = await User.findById(userId);
+  
+  if(!user?.phone || !user?.address ){
+    throw new AppError(httpStatusCodes.BAD_REQUEST, "Please update your profile to book a tour");
+  }
+  
+  const tour = await Tour.findById(payload.tour).select("costFrom");
+
+  if(!tour?.costFrom){
+    throw new AppError(httpStatusCodes.BAD_REQUEST, "No tour cost found.")
+  }
+
+  const amount = Number(tour.costFrom) * Number(payload.guestCount);
+
+  const booking = await Booking.create({
+    user: userId,
+    status: EBookingStatus.PENDING,
+    ...payload
+  })
+
+  const payment = await Payment.create({
+    booking: booking._id,
+    status: EPaymentStatus.UNPAID,
+    transactionId: transactionId,
+    amount: amount
+  })
+
+  const updatedBooking = await Booking.findByIdAndUpdate(booking._id, {payment: payment._id}, {new: true, runValidators: true})
+  
+  return {}
 };
 const getUserBookings = async () => {
 
