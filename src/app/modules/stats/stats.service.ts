c7dcc9b120fa50646500ecf7f2b135a87fe3023a
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-09-02 15:45:03 +0600

    Boilerplate of stats route, controller and service created

---
+++ src/app/modules/stats/stats.service.ts
@@ -0,0 +1,27 @@
+
+const getUserStats = async () => {
+    return {}
+}
+
+const getTourStats = async () => {
+    return {}
+}
+
+const getBookingStats = async () => {
+    
+    return {}
+}
+
+const getPaymentStats = async () => {
+
+    
+    return { }
+}
+
+
+export const StatsService = {
+    getBookingStats,
+    getPaymentStats,
+    getTourStats,
+    getUserStats
+}
